aa56d75edce8675b386e150d4ae8a6cf3ecb5ff3
maria-aress <maria@aress.es>
2023-08-22 08:31:55 +0200

    Primer commit

---
+++ assets/js/settings-init.js
@@ -0,0 +1,13 @@
+
+"use strict"
+var optionSettings = {
+    layout:"wide",
+    background:"origin",
+    color:"blue",
+    header:"static",
+    font:"opensans",
+    textDirection:"ltr",
+    radius:"defaultradius",
+    showSettings:"hide",
+};
+new antlerSettings(optionSettings);
